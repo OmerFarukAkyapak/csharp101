8844a61b2f1d77b2b30d35bd86197ee36dc69371
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-10-03 14:12:13 +0300

    created console programming

---
+++ console_programming/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace console_programming;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello,You are in the console programming.");
+        Console.WriteLine("Whats your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello " + name + "!");
+
+        Console.ReadLine();
+    }
+}
