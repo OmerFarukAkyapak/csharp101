62d2e263c50a2d2545352473375fe94c92200f07
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-10-03 14:29:51 +0300

    added diffrent veriable types example

--- veriables/Program.cs
+++ veriables/Program.cs
@@ -6,7 +6,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int number = 10; // integer type veriable
+
+        string name = "John"; // string type veriable
+        string Name = "Alphie"; // string type veriable, diffrant from name because of capital N
+
+        bool isWorking = true; // boolean type veriable
+
+        double pi = 3.14; // double type veriable
+
+        char letter = 'A'; // char type veriable
+
+
+        Console.WriteLine("integer: " + number);
+        Console.WriteLine("string with name: " + name);
+        Console.WriteLine("string with Name: " + Name);
+        Console.WriteLine("boolean: " + isWorking);
+        Console.WriteLine("double: " + pi);
+        Console.WriteLine("char: " + letter);
+
+
+
         Console.ReadLine();
     }
 }
