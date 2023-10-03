551883e14b2ab3dd42eafeb0fdcfd3a9a8cb3b60
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-10-03 15:55:52 +0300

    added switch case condition

--- conditions_loops/Program.cs
+++ conditions_loops/Program.cs
@@ -6,14 +6,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        // If-Else
-        int timeNow = DateTime.Now.Hour;
+        DateTime time = DateTime.Now;
+        Console.WriteLine("Your time: " + time);
 
-        if (timeNow >= 6 && timeNow < 12)
+
+        // If-Else
+        int hour = time.Hour;
+
+        if (hour >= 6 && hour < 12)
         {
             Console.WriteLine("Good Morning!");
         }
-        else if (timeNow >= 12 && timeNow < 18)
+        else if (hour >= 12 && hour < 18)
         {
             Console.WriteLine("Good Afternoon!");
         }
@@ -21,6 +25,42 @@ class Program
         {
             Console.WriteLine("Good Evening!");
         }
+
+        // Switch
+        int month = time.Month;
+
+        switch (month)
+        {
+            case 12:
+            case 1:
+            case 2:
+                Console.WriteLine("Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                Console.WriteLine("Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                Console.WriteLine("Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                Console.WriteLine("Fall");
+                break;
+            default:
+                Console.WriteLine("Unknown Season");
+                break;
+        }
+
+
+
+
+
+
 
         Console.ReadLine();
     }
