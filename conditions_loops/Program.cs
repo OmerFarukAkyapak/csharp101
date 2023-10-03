c84963434663d52823e8624e65cb63e11328ef47
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-10-03 15:39:34 +0300

    added if-else conditions

--- conditions_loops/Program.cs
+++ conditions_loops/Program.cs
@@ -6,6 +6,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // If-Else
+        int timeNow = DateTime.Now.Hour;
+
+        if (timeNow >= 6 && timeNow < 12)
+        {
+            Console.WriteLine("Good Morning!");
+        }
+        else if (timeNow >= 12 && timeNow < 18)
+        {
+            Console.WriteLine("Good Afternoon!");
+        }
+        else
+        {
+            Console.WriteLine("Good Evening!");
+        }
+
+        Console.ReadLine();
     }
 }
