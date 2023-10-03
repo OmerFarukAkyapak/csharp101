414094edfcb60514b90fefc9a3ad0379a5a76712
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-10-03 14:56:48 +0300

    created operations

---
+++ operations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace operations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+    }
+}
