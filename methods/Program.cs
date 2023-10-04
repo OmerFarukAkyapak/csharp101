d4e2cf72d3da1c4fd6edb8d19bdeb6a4c9ffe3d6
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-10-04 14:40:10 +0300

    added arrays methods

--- methods/Program.cs
+++ methods/Program.cs
@@ -8,7 +8,36 @@ class Program
     {
         //methods
         //methods are a way to group code together
-        Console.WriteLine("hii");
+
+        //array
+        int[] numbers = { 23, 41, 3, 32, 51, 16, 7, 11 };
+        Console.WriteLine("Numbers:");
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("");
+        //sort
+        Console.WriteLine("After Sort");
+        Array.Sort(numbers);
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("");
+        //reverse
+        Console.WriteLine("After Reverse");
+        Array.Reverse(numbers);
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("");
+
+
+
+
+
         Console.ReadLine();
 
     }
