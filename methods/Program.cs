50be8df68ff7e2b69d41302194960656eaed9412
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-10-04 14:32:24 +0300

    created method folder

---
+++ methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace methods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //methods
+        //methods are a way to group code together
+        Console.WriteLine("hii");
+        Console.ReadLine();
+
+    }
+}
