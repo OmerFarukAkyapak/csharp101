9afc3331c44ccaa5ae10b971a12273a1a0dd8604
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-10-05 18:23:50 +0300

    created oop examples project

---
+++ oop_examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_examples;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+    }
+}
